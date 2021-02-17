8fb1bb60c510c0800f8e062d71f3b6458c548915
Ruben <ruben.iglesias@sotec.es>
2021-02-17 16:55:47 +0100

    Three checkboxes complete/incomplete/na

--- projects/views.py
+++ projects/views.py
@@ -10,8 +10,6 @@ import os
 from io import BytesIO
 from reportlab.pdfgen import canvas
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Image
-
-
 def load_json_file(level):
     results = []
     with open('common/asvs.json') as f:
@@ -115,6 +113,7 @@ def project_view(request, projectid):
     project = load_template(phash)
     if project['project_owner'] == request.user.username:
         percentage = calculate_completion(project['requirements'])
+    
         return render(request, "projects/view.html", {'data': project['requirements'], 'project': project, 'percentage': percentage})
 
 
@@ -129,11 +128,20 @@ def project_update(request):
                 pass
             else:
                 for r in project['requirements']:
-                    if r['Item'] in k:
-                        if int(v) == 0:
-                            r['enabled'] = 0
-                        else:
-                            r['enabled'] = 1
+                    if request.POST.get(r['Item']+'enabled')=="1":
+                        r['enabled'] = 1  
+                    else:
+                        r['enabled'] = 0  
+                    if request.POST.get(r['Item']+'disabled')=="1":
+                        r['disabled'] = 1  
+                    else:
+                        r['disabled'] = 0  
+                    if request.POST.get(r['Item']+'na')=="1":
+                        r['enabled'] = 0  
+                        r['disabled'] = 0  
+                       
+                    
+
         update_template(phash, project)
         return redirect('projectsview', projectid=request.POST.get('projectid'))
 
@@ -186,9 +194,12 @@ def generate_pdf(request, projectid):
         if r.get('enabled') and r['enabled'] > 0:
             data.append(["Complete ✓"])
             data.append([" "])
-        else:
+        elif r.get('disabled') and r['disabled'] > 0:
             data.append(["Incomplete ✕"])  
             data.append([" "])
+        else:  
+            data.append(["N/A "])  
+            data.append([" "])  
     
   
     maxlength=0
