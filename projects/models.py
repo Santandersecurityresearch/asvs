b6f00fa32f0979087898c3c7de561c010b92612c
danielcuthbert <daniel.cuthbert@gruposantander.com>
2021-02-23 11:53:30 +0000

    Cleaned up unused variables
    Cleaned up a handful of unused variables and imports

--- projects/models.py
+++ projects/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db.models import Q
 from django.contrib.auth.models import User
-from django.urls import reverse
 
 
 class ProjectQuerySet(models.QuerySet):
