03fe7c60411c5aba962325db392008874c203aa5
Ruben <ruben.iglesias@sotec.es>
2021-03-04 15:00:22 +0100

    .

--- projects/models.py
+++ projects/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models import Q
 from django.contrib.auth.models import User
-from django.contrib.postgres.fields import ArrayField
+
 
 
 class ProjectQuerySet(models.QuerySet):
