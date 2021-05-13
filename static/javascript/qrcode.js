f0456ee416e438bfbee6ccd1790fa649e3ddcd3b
Ruben <ruben.iglesias@sotec.es>
2021-05-13 12:27:21 +0200

    External links noted with an icon and opened in new tab

--- static/javascript/qrcode.js
+++ static/javascript/qrcode.js
@@ -4,7 +4,7 @@
  * - Fixed dataset of 'QRCode for Javascript library' for support full-spec.
  * - this library has no dependencies.
  * 
- * @author davidshimjs
+
  * @see <a href="http://www.d-project.com/" target="_blank">http://www.d-project.com/</a>
  * @see <a href="http://jeromeetienne.github.com/jquery-qrcode/" target="_blank">http://jeromeetienne.github.com/jquery-qrcode/</a>
  */
