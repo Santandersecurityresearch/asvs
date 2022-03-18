3c410e73ca0083aaabd269227ea9798b40bd4d05
Daniel Cuthbert <daniel@seventysix.io>
2022-03-18 10:26:14 +0000

    cleaned up for django 4 and static content sharing
    Signed-off-by: Daniel Cuthbert <daniel@seventysix.io>

--- asvs/settings.py
+++ asvs/settings.py
@@ -116,10 +116,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
